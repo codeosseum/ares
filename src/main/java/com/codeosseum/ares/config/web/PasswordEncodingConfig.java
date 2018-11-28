c4115df836e769469dfc2deaf2d841e1f027a5d7
Attila Bagossy <bagossyattila@outlook.com>
2018-11-28 15:44:15 +0100

    Set the password encoder implementation to BCrypt

---
+++ src/main/java/com/codeosseum/ares/config/web/PasswordEncodingConfig.java
@@ -0,0 +1,16 @@
+package com.codeosseum.ares.config.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordEncodingConfig {
+    private static final int ENCODING_STRENGTH = 10;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(ENCODING_STRENGTH);
+    }
+}
