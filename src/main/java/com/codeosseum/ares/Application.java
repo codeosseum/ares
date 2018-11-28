78085e428633161dda399bc0866c7886b2b4fb87
Attila Bagossy <bagossyattila@outlook.com>
2018-11-28 14:01:32 +0100

    Application now acts as the entry point of the Spring application

--- src/main/java/com/codeosseum/ares/Application.java
+++ src/main/java/com/codeosseum/ares/Application.java
@@ -1,7 +1,11 @@
 package com.codeosseum.ares;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello, Ares!");
+        SpringApplication.run(Application.class, args);
     }
 }
