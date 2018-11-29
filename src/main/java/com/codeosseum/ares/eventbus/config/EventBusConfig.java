127d2bf8552936f1d8466f03ebe1bbfdb863cdf4
Attila Bagossy <bagossyattila@outlook.com>
2018-11-29 23:57:34 +0100

    Created a basic configuration for the EventBus

---
+++ src/main/java/com/codeosseum/ares/eventbus/config/EventBusConfig.java
@@ -0,0 +1,23 @@
+package com.codeosseum.ares.eventbus.config;
+
+import com.google.common.eventbus.AsyncEventBus;
+import com.google.common.eventbus.EventBus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+@Configuration
+public class EventBusConfig {
+    private static final int EXECUTOR_THREAD_COUNT = 2;
+
+    @Bean
+    public EventBus eventBus() {
+        return new AsyncEventBus(eventBusExecutor());
+    }
+
+    private Executor eventBusExecutor() {
+        return Executors.newFixedThreadPool(EXECUTOR_THREAD_COUNT);
+    }
+}
