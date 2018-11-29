8c8050e40d753f6ac29b32d5cd6b58628330ea21
Attila Bagossy <bagossyattila@outlook.com>
2018-11-29 19:56:32 +0100

    Fixed the authentications service: now it correctly detects logged in and anonymous users

--- src/main/java/com/codeosseum/ares/security/util/DefaultAuthenticationServiceImpl.java
+++ src/main/java/com/codeosseum/ares/security/util/DefaultAuthenticationServiceImpl.java
@@ -3,6 +3,8 @@ package com.codeosseum.ares.security.util;
 import com.codeosseum.ares.security.login.UserDetails;
 import com.codeosseum.ares.user.Role;
 import com.codeosseum.ares.user.User;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
@@ -24,8 +26,14 @@ public class DefaultAuthenticationServiceImpl implements AuthenticationService {
 
     @Override
     public Optional<User> getAuthenticatedUser() {
-         return Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication().getPrincipal())
+        return Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication())
+                 .filter(this::isLoggedInAuthentication)
+                 .map(Authentication::getPrincipal)
                  .map(obj -> (UserDetails)obj)
                  .map(UserDetails::getUser);
+    }
+
+    private boolean isLoggedInAuthentication(final Authentication authentication) {
+        return !(authentication instanceof AnonymousAuthenticationToken);
     }
 }
