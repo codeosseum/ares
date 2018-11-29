d2dceb1436c3f88a6613f4466094b09f36b6a9c2
Attila Bagossy <bagossyattila@outlook.com>
2018-11-29 18:38:31 +0100

    Registered the custom UserDetailsService

--- src/main/java/com/codeosseum/ares/config/web/WebSecurityConfig.java
+++ src/main/java/com/codeosseum/ares/config/web/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.springframework.boot.autoconfigure.security.StaticResourceLocation.WEB_JARS;
@@ -19,6 +20,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
         this.configureRequestAccess(http)
@@ -28,9 +32,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .passwordEncoder(passwordEncoder)
-                .withUser("user").password(passwordEncoder.encode("1234")).roles("USER");
+        auth.userDetailsService(userDetailsService);
     }
 
     private WebSecurityConfig configureRequestAccess(final HttpSecurity http) throws Exception {
