1c12621477d99e7803e295ba8529aed9d22489a9
Attila Bagossy <bagossyattila@outlook.com>
2018-12-02 18:52:42 +0100

    Added necessary dependencies to the MatchmakingController

--- src/main/java/com/codeosseum/ares/matchmaking/foundation/controller/MatchmakingController.java
+++ src/main/java/com/codeosseum/ares/matchmaking/foundation/controller/MatchmakingController.java
@@ -1,13 +1,26 @@
 package com.codeosseum.ares.matchmaking.foundation.controller;
 
+import com.codeosseum.ares.matchmaking.foundation.matchmaker.MatchmakerFacade;
+import com.codeosseum.ares.security.util.AuthenticationService;
 import com.codeosseum.ares.web.Paths;
 import com.codeosseum.ares.web.Views;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class MatchmakingController {
+    private final AuthenticationService authenticationService;
+
+    private final MatchmakerFacade matchmakerFacade;
+
+    public MatchmakingController(AuthenticationService authenticationService, MatchmakerFacade matchmakerFacade) {
+        this.authenticationService = authenticationService;
+        this.matchmakerFacade = matchmakerFacade;
+    }
+
     @GetMapping(Paths.Game.MATCHMAKING)
     public String getMatchmakingPage() {
         return Views.Game.MATCHMAKING;
