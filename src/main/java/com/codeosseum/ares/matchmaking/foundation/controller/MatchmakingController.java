832dac01efb6a15d98364c42d5131d51c5fc1325
Attila Bagossy <bagossyattila@outlook.com>
2018-12-02 19:06:36 +0100

    Implemented the request handlers in MatchmakingController

--- src/main/java/com/codeosseum/ares/matchmaking/foundation/controller/MatchmakingController.java
+++ src/main/java/com/codeosseum/ares/matchmaking/foundation/controller/MatchmakingController.java
@@ -2,6 +2,7 @@ package com.codeosseum.ares.matchmaking.foundation.controller;
 
 import com.codeosseum.ares.matchmaking.foundation.matchmaker.MatchmakerFacade;
 import com.codeosseum.ares.security.util.AuthenticationService;
+import com.codeosseum.ares.user.User;
 import com.codeosseum.ares.web.Paths;
 import com.codeosseum.ares.web.Views;
 import org.springframework.http.HttpStatus;
@@ -9,6 +10,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import static org.springframework.http.HttpStatus.NO_CONTENT;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 @Controller
 public class MatchmakingController {
@@ -28,11 +32,27 @@ public class MatchmakingController {
 
     @GetMapping(Paths.Api.MATCHMAKING)
     public ResponseEntity checkForMatch() {
-        return new ResponseEntity(HttpStatus.OK);
+        if (!authenticationService.hasAuthenticatedUser()) {
+            return new ResponseEntity(UNAUTHORIZED);
+        }
+
+        return authenticationService.getAuthenticatedUser()
+                .map(User::getUsername)
+                .flatMap(matchmakerFacade::getMatchForPlayer)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> new ResponseEntity<>(NO_CONTENT));
     }
 
     @DeleteMapping(Paths.Api.MATCHMAKING)
     public ResponseEntity removeFromMatchmaking() {
+        if (!authenticationService.hasAuthenticatedUser()) {
+            return new ResponseEntity(UNAUTHORIZED);
+        }
+
+        authenticationService.getAuthenticatedUser()
+                .map(User::getUsername)
+                .ifPresent(matchmakerFacade::removePlayerFromMatchmaking);
+
         return new ResponseEntity(HttpStatus.OK);
     }
 }
