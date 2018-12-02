1541f8bf8c8b3f045c5fbdcdd24e7f1ac19dcb1d
Attila Bagossy <bagossyattila@outlook.com>
2018-12-02 19:51:41 +0100

    Implemented the MatchmakingFacade methods

--- src/main/java/com/codeosseum/ares/matchmaking/foundation/MatchmakingFacade.java
+++ src/main/java/com/codeosseum/ares/matchmaking/foundation/MatchmakingFacade.java
@@ -1,17 +1,32 @@
 package com.codeosseum.ares.matchmaking.foundation;
 
 import com.codeosseum.ares.matchmaking.foundation.matchmaker.AssignedMatch;
+import com.codeosseum.ares.matchmaking.foundation.matchmaker.Matchmaker;
+import com.codeosseum.ares.matchmaking.foundation.matchmaker.MatchmakingProfile;
+import com.codeosseum.ares.matchmaking.foundation.matchmaker.MatchmakingResult;
+import com.codeosseum.ares.matchmaking.foundation.notificator.PlayerNotificator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
 public class MatchmakingFacade {
+    @Autowired
+    private PlayerNotificator playerNotificator;
+
+    @Autowired
+    private List<Matchmaker<? extends MatchmakingProfile, ? extends MatchmakingResult>> matchmakers;
+
     public Optional<AssignedMatch> getMatchForPlayer(final String username) {
-        return Optional.empty();
+        return playerNotificator.getMatchForPlayer(Objects.requireNonNull(username));
     }
 
     public void removePlayerFromMatchmaking(final String username) {
+        Objects.requireNonNull(username);
 
+        matchmakers.forEach(matchmaker -> matchmaker.removePlayerFromMatchmaking(username));
     }
 }
