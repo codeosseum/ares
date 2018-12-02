4366de9c43bb0ce8593c3ca26bf2ad42f4dccf25
Attila Bagossy <bagossyattila@outlook.com>
2018-12-02 18:48:38 +0100

    Config setting up MatchMadeEvent in the EventRegistry

---
+++ src/main/java/com/codeosseum/ares/matchmaking/foundation/config/EventConfig.java
@@ -0,0 +1,25 @@
+package com.codeosseum.ares.matchmaking.foundation.config;
+
+import com.codeosseum.ares.eventbus.dispatch.EventDispatcher;
+import com.codeosseum.ares.eventbus.registry.EventRegistry;
+import com.codeosseum.ares.matchmaking.foundation.matchmaker.MatchMadeEvent;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+public class EventConfig {
+    private final EventDispatcher eventDispatcher;
+
+    private final EventRegistry eventRegistry;
+
+    public EventConfig(EventDispatcher eventDispatcher, EventRegistry eventRegistry) {
+        this.eventDispatcher = eventDispatcher;
+        this.eventRegistry = eventRegistry;
+    }
+
+    @PostConstruct
+    public void registerEvents() {
+        eventRegistry.registerEvent(MatchMadeEvent.IDENTIFIER, MatchMadeEvent.class);
+    }
+}
