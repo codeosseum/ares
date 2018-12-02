87f7ea1576f8b4ebb423045cd272f3e69165c387
Attila Bagossy <bagossyattila@outlook.com>
2018-12-02 23:32:04 +0100

    Additional configuration for matchmakers

---
+++ src/main/java/com/codeosseum/ares/config/MatchmakingConfig.java
@@ -0,0 +1,19 @@
+package com.codeosseum.ares.config;
+
+import com.codeosseum.ares.matchmaking.foundation.matchmaker.MatchConfiguration;
+import com.codeosseum.ares.matchmaking.foundation.matchmaker.Matchmaker;
+import com.codeosseum.ares.matchmaking.foundation.matchmaker.MatchmakingProfile;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+import static java.util.Collections.emptyList;
+
+@Configuration
+public class MatchmakingConfig {
+    @Bean
+    public List<Matchmaker<? extends MatchmakingProfile, ? extends MatchConfiguration>> matchmakers() {
+        return emptyList();
+    }
+}
