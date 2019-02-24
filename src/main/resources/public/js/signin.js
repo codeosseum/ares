5f1d6e92d2ca3f12bd8fd1537246120a92dfab87
Attila Bagossy <bagossyattila@outlook.com>
2019-02-24 18:02:05 +0100

    Frontend renamings and Sign Up page revamp

--- src/main/resources/public/js/signin.js
+++ src/main/resources/public/js/signin.js
@@ -1,9 +1,9 @@
-(function loginIIFE() {
+(function signInIIFE() {
     const usernameField = document.getElementsByName('username')[0];
 
-    document.getElementById('login-form').onsubmit = onLoginFormSubmit;
+    document.getElementsByClassName('sign-in-form')[0].onsubmit = onSignInFormSubmit;
 
-    function onLoginFormSubmit() {
+    function onSignInFormSubmit() {
         window.sessionStorage.setItem('username', usernameField.value);
 
         console.log(window.sessionStorage.getItem('username'));
--- src/main/resources/public/js/signup.js
+++ src/main/resources/public/js/signup.js
@@ -1,31 +1,31 @@
-(function registrationIIFE() {
+(function signUpIIFE() {
     const usernameField = document.getElementsByName('username')[0];
     const passwordField = document.getElementsByName('password')[0];
     const emailField = document.getElementsByName('email')[0];
 
-    document.getElementsByClassName('register-button')[0].addEventListener('click', initiateRegistration)
+    document.getElementsByClassName('sign-up-button')[0].addEventListener('click', initiateSignUp);
 
-    async function initiateRegistration() {
-        const registrationRequest = {
+    async function initiateSignUp() {
+        const signUpRequest = {
             username: usernameField.value,
             password: passwordField.value,
             email: emailField.value,
         };
-        
-        const response = await fetch('/api/registration', {
+
+        const response = await fetch('/api/signup', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json; charset=utf-8'
             },
-            body: JSON.stringify(registrationRequest)
+            body: JSON.stringify(signUpRequest)
         });
         
         if (response.ok) {
-            window.location = '/login';
+            window.location = '/signin';
         } else {
             const failure = await response.json();
 
             console.log(failure);
         }
-    }
+    };
 })();
