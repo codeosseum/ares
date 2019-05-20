c53394693069f6c995fad6b2b335114d60d55fd3
Attila Bagossy <bagossyattila@outlook.com>
2019-05-20 10:09:02 +0200

    Matchmaking JS updated

--- src/main/resources/public/js/matchmaking.js
+++ src/main/resources/public/js/matchmaking.js
@@ -1,5 +1,12 @@
 (function matchmakingIIFE() {
-    document.getElementById('find-match-button').addEventListener('click', findMatch)
+    document.querySelector('.action-button.find-fault-seeding-match')
+        .addEventListener('click', findFaultSeedingMatch);
+
+    document.querySelector('.accept-match-button')
+        .addEventListener('click', acceptMatch);
+
+    const username = window.sessionStorage.getItem('username');
+    let match = null;
 
     let intervalHandle;
 
@@ -11,36 +18,54 @@
         });
 
         if (response.status == 204) {
-            console.log('no match');
+            console.log('no match yet');
         } else {
-            const match = await response.json();
+            match = await response.json();
 
             console.log(match);
 
-            const uri = `ws://${match.server.uri.split('://')[1]}/ws`;
-
-            ws = new WebSocket(uri);
-
-            ws.addEventListener('open', () => {
-                const hello = {
-                    action: 'hello',
-                    payload: {
-                        username: window.sessionStorage.getItem('username')
-                    }
-                };
-
-                ws.send(JSON.stringify(hello));
-            })
-
-            ws.addEventListener('message', event => {
-                console.log(JSON.parse(event.data));
-            })
-
-            clearInterval(intervalHandle);
+            matchFound();
         }
-    }
+    };
 
-    async function findMatch() {
+    async function matchFound() {
+        document.querySelector('.finding-match-status').classList.add('hidden');
+        document.querySelector('.match-found-status').classList.remove('hidden');
+        document.querySelector('.accept-match-button').classList.remove('visibility-hidden');
+
+        clearInterval(intervalHandle);
+    };
+
+    async function acceptMatch() {
+        window.sessionStorage.setItem('match', JSON.stringify(match));
+        
+        window.location = '/game/fault-seeding'
+        /*const uri = `ws://${match.server.uri.split('://')[1]}/ws`;
+
+        ws = new WebSocket(uri);
+
+        ws.addEventListener('open', () => {
+            const hello = {
+                action: 'hello',
+                payload: {
+                    username
+                }
+            };
+
+            ws.send(JSON.stringify(hello));
+        })
+
+        ws.addEventListener('message', event => {
+            console.log(JSON.parse(event.data));
+        })*/
+    };
+
+    async function findFaultSeedingMatch() {
+        document.querySelector('.overlay').classList.remove('hidden');
+        document.querySelector('.finding-match-status').classList.remove('hidden');
+        document.querySelector('.match-found-status').classList.add('hidden');
+        document.querySelector('.accept-match-button').classList.add('visibility-hidden');
+
         const response = await fetch('/api/matchmaking/two-player-fault-seeding', {
             method: 'POST'
         });
@@ -52,5 +77,5 @@
 
             console.log(failure);
         }
-    }
+    };
 })();
